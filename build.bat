6d35fa829398c60c23186d160849af6bd13f57be
SiYuan <863909694@qq.com>
2024-04-13 14:16:57 +0800

    新增两个脚本

---
+++ build.bat
@@ -0,0 +1,16 @@
+@echo off
+set "source_folder=E:\Project\web\MemoTraceWiki\src\posts"
+set "destination_folder=E:\Project\Python\Langchain-Chatchat\knowledge_base\MemoTrace\content"
+call npm run docs:build
+echo Copying .md files from %source_folder% to %destination_folder%...
+
+rem 创建目标文件夹
+mkdir %destination_folder% 2>nul
+
+rem 复制文件
+for /r "%source_folder%" %%f in (*.md) do (
+    copy "%%f" "%destination_folder%" >nul
+    echo "%%f"
+)
+
+echo Copy completed.
