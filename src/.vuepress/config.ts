0cfe41987e99f44fc9db485372e0fa58ea32b8aa
AliYun8g <863909694@qq.com>
2025-01-05 21:00:18 +0800

    修改AI地址

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -17,7 +17,7 @@ export default defineUserConfig({
       {
         async: true,
         defer: true,
-        src: 'http://kbchat.lc044.love/api/application/embed?protocol=http&host=kbchat.lc044.love&token=16518b367388aa75'
+        src: 'https://chat.memotrace.cn/api/application/embed?protocol=https&host=chat.memotrace.cn&token=16518b367388aa75'
       }
     ]
   ],
